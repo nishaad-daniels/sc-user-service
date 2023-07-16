8166311fe04bdfd54f122680ca2409a467568dbd
Nishaad Daniels <nishaaddaniels01@gmail.com>
2023-07-17 01:02:48 +0200

    Initial Spring Commit

---
+++ src/test/java/za/co/nishaad/microservices/demo/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.nishaad.microservices.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
